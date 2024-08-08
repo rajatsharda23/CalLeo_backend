ba77b1063ff5cc64c608b1d7a07df1c4bfe72fde
rsharda23 <72014316+rajatsharda23@users.noreply.github.com>
2024-08-08 17:39:32 +0530

    unable to get google auth

--- routers/auth.route.js
+++ routers/auth.route.js
@@ -2,41 +2,64 @@ const express = require('express');
 const { google } = require('googleapis');
 require('dotenv').config();
 const router = express.Router();
+const cors = require("cors");
+
+
+router.use(cors())
 
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
 
-const oauth2Client = new google.auth.OAuth2(
-  GOOGLE_CLIENT_ID,
-  GOOGLE_CLIENT_SECRET,
-  REDIRECT_URI
-);
 
-// Test route to ensure the auth route is working
-router.get('/test', (req, res) => {
-  res.send({ message: "Auth API working" });
+// const oauth2Client = new google.auth.OAuth2(
+//   GOOGLE_CLIENT_ID,
+//   GOOGLE_CLIENT_SECRET,
+//   REDIRECT_URI,
+//   'authorization_code',
+// );
+
+
+
+const googleAuth = new GoogleAuth({
+  credentials: {
+    client_id: GOOGLE_CLIENT_ID,
+    client_secret: GOOGLE_CLIENT_SECRET,
+    redirect_uri: REDIRECT_URI,
+  },
+  scopes: ['https://www.googleapis.com/auth/calendar'],
 });
 
-// Route to initiate Google OAuth
-router.get('/google', (req, res) => {
-  const authUrl = oauth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email'],
-  });
-  res.redirect(authUrl);
-});
 
-// Route to handle OAuth2 callback and create tokens
-router.get('/google/callback', async (req, res, next) => {
+router.post('/create-tokens', async (req, res) => {
   try {
-    const { code } = req.query;
+    const { code } = req.body;
+    const oauth2Client = new google.auth.OAuth2(
+      GOOGLE_CLIENT_ID,
+      GOOGLE_CLIENT_SECRET,
+      REDIRECT_URI
+    );
     const { tokens } = await oauth2Client.getToken(code);
     oauth2Client.setCredentials(tokens);
-    res.json(tokens); // You can store these tokens in the session or database
-  } catch (error) {
-    next(error);
+    res.json(tokens);
+  } catch (err) {
+    console.error('Error exchanging authorization code for tokens:', err);
+    res.status(400).send({
+      error: 'Failed to exchange authorization code for tokens',
+      details: err.response ? err.response.data : err.message,
+    });
   }
 });
+
+router.post('/create-tokens/refresh-token', async (req, res) => {
+  const user = new UserRefreshClient(
+    GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET,
+    req.body.refreshToken,
+  );
+  const { credentials } = await user.refreshAccessToken(); // optain new tokens
+  res.json(credentials);
+})
+
 
 module.exports = router;
